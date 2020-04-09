17baef185c74a24b362656146da1ade95240a141
Shingo INADA <mecha.is06@gmail.com>
2020-04-09 09:01:34 +0900

    set loginkey to telephone number

--- build_run.sh
+++ build_run.sh
@@ -7,8 +7,10 @@ cd $(dirname "$0")
 
 make
 
-export GDXSV_LOBBY_PUBLIC_ADDR="localhost:3333"
-export GDXSV_LOBBY_ADDR="localhost:3333"
+export GDXSV_LOBBY_PUBLIC_ADDR="192.168.0.10:9876"
+export GDXSV_LOBBY_ADDR="localhost:9876"
+export GDXSV_BATTLE_PUBLIC_ADDR="192.168.0.10:9877"
+export GDXSV_BATTLE_ADDR="localhost:9877"
 
 ./bin/gdxsv -dump -v 3 lobby 2>&1 | tee log.txt
 
