cbbb146376893ad44660a8bc99d660cc5208739b
Shingo INADA <mecha.is06@gmail.com>
2020-04-15 08:40:02 +0900

    update flycasts

--- run4flycast.sh
+++ run4flycast.sh
@@ -8,8 +8,26 @@ cd $(dirname "$0")
 #readonly GDX_ROM_PATH=${GDX_ROM_PATH:-'C:\rom\gdx-disc2\gdx-disc2.gdi'}
 
 mkdir -p work/bin
-## TODO download
-if [[ -f ~/Downloads/flycast.zip  ]]; then
+
+ls ./flycast/shell/linux/artifacts || true
+ls ~/Downloads/flycast.zip || true
+
+if [[ -f ./flycast/shell/linux/artifacts/flycast.exe ]]; then
+  set +x
+  echo "======================="
+  echo "USE local build version"
+  echo "======================="
+  sleep 2
+  set -x
+  cp ./flycast/shell/linux/artifacts/flycast.exe work/bin/flycast.exe
+elif [[ -f ~/Downloads/flycast.zip  ]]; then
+  set +x
+  echo "======================="
+  echo "USE ci build version"
+  echo "======================="
+  sleep 2
+  set -x
+  ## TODO download
   mv ~/Downloads/flycast.zip ./work/bin/
   pushd work/bin
     unzip flycast.zip
@@ -26,4 +44,7 @@ trap 'kill $(jobs -p)' EXIT
 for i in 1 2 3 4; do
   cd work/flycast${i} && MSYS_NO_PATHCONV=1 ./flycast.exe &
 done
+
+rm -f work/flycast1/flycast.log
+tail -F work/flycast1/flycast.log &
 wait $(jobs -l %1 | awk '{print $2}')
