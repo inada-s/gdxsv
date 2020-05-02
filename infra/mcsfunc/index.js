1752b002496b241b25c787ec84ce3ea5f7c004c9
Shingo INADA <mecha.is06@gmail.com>
2020-05-02 11:24:27 +0900

    update mcsfunc

--- infra/mcsfunc/index.js
+++ infra/mcsfunc/index.js
@@ -1,33 +1,36 @@
 'use strict';
 
+const Compute = require('@google-cloud/compute');
+const url = require('url');
+
 // https://cloud.google.com/compute/docs/regions-zones
 const gcpRegions = {
-  "asia-east1": { "zones": ["a", "b", "c"], "location": "Changhua County, Taiwan" },
-  "asia-east2": { "zones": ["a", "b", "c"], "location": "Hong Kong" },
-  "asia-northeast1": { "zones": ["a", "b", "c"], "location": "Tokyo, Japan" },
-  "asia-northeast2": { "zones": ["a", "b", "c"], "location": "Osaka, Japan" },
-  "asia-northeast3": { "zones": ["a", "b", "c"], "location": "Seoul, South Korea" },
-  "asia-south1": { "zones": ["a", "b", "c"], "location": "Mumbai, India" },
-  "asia-southeast1": { "zones": ["a", "b", "c"], "location": "Jurong West, Singapore" },
-  "australia-southeast1": { "zones": ["a", "b", "c"], "location": "Sydney, Australia" },
-  "europe-north1": { "zones": ["a", "b", "c"], "location": "Hamina, Finland" },
-  "europe-west1": { "zones": ["b", "c", "d"], "location": "St. Ghislain, Belgium" },
-  "europe-west2": { "zones": ["a", "b", "c"], "location": "London, England, UK" },
-  "europe-west3": { "zones": ["a", "b", "c"], "location": "Frankfurt, Germany" },
-  "europe-west4": { "zones": ["a", "b", "c"], "location": "Eemshaven, Netherlands" },
-  "europe-west6": { "zones": ["a", "b", "c"], "location": "Zürich, Switzerland" },
-  "northamerica-northeast1": { "zones": ["a", "b", "c"], "location": "Montreal, Quebec, Canada" },
-  "southamerica-east1": { "zones": ["a", "b", "c"], "location": "Osasco (Sao Paulo), Brazil" },
-  "us-central1": { "zones": ["a", "b", "c", "f"], "location": "Council Bluffs, Iowa, USA" },
-  "us-east1": { "zones": ["b", "c", "d"], "location": "Moncks Corner, South Carolina, USA" },
-  "us-east4": { "zones": ["a", "b", "c"], "location": "Ashburn, Northern Virginia, USA" },
-  "us-west1": { "zones": ["a", "b", "c"], "location": "The Dalles, Oregon, USA" },
-  "us-west2": { "zones": ["a", "b", "c"], "location": "Los Angeles, California, USA" },
-  "us-west3": { "zones": ["a", "b", "c"], "location": "Salt Lake City, Utah, USA" },
+    "asia-east1": {"zones": ["a", "b", "c"], "location": "Changhua County, Taiwan"},
+    "asia-east2": {"zones": ["a", "b", "c"], "location": "Hong Kong"},
+    "asia-northeast1": {"zones": ["a", "b", "c"], "location": "Tokyo, Japan"},
+    "asia-northeast2": {"zones": ["a", "b", "c"], "location": "Osaka, Japan"},
+    "asia-northeast3": {"zones": ["a", "b", "c"], "location": "Seoul, South Korea"},
+    "asia-south1": {"zones": ["a", "b", "c"], "location": "Mumbai, India"},
+    "asia-southeast1": {"zones": ["a", "b", "c"], "location": "Jurong West, Singapore"},
+    "australia-southeast1": {"zones": ["a", "b", "c"], "location": "Sydney, Australia"},
+    "europe-north1": {"zones": ["a", "b", "c"], "location": "Hamina, Finland"},
+    "europe-west1": {"zones": ["b", "c", "d"], "location": "St. Ghislain, Belgium"},
+    "europe-west2": {"zones": ["a", "b", "c"], "location": "London, England, UK"},
+    "europe-west3": {"zones": ["a", "b", "c"], "location": "Frankfurt, Germany"},
+    "europe-west4": {"zones": ["a", "b", "c"], "location": "Eemshaven, Netherlands"},
+    "europe-west6": {"zones": ["a", "b", "c"], "location": "Zürich, Switzerland"},
+    "northamerica-northeast1": {"zones": ["a", "b", "c"], "location": "Montreal, Quebec, Canada"},
+    "southamerica-east1": {"zones": ["a", "b", "c"], "location": "Osasco (Sao Paulo), Brazil"},
+    "us-central1": {"zones": ["a", "b", "c", "f"], "location": "Council Bluffs, Iowa, USA"},
+    "us-east1": {"zones": ["b", "c", "d"], "location": "Moncks Corner, South Carolina, USA"},
+    "us-east4": {"zones": ["a", "b", "c"], "location": "Ashburn, Northern Virginia, USA"},
+    "us-west1": {"zones": ["a", "b", "c"], "location": "The Dalles, Oregon, USA"},
+    "us-west2": {"zones": ["a", "b", "c"], "location": "Los Angeles, California, USA"},
+    "us-west3": {"zones": ["a", "b", "c"], "location": "Salt Lake City, Utah, USA"},
 }
 
 function getStartupScript(version) {
-  return `\
+    return `\
 #!/bin/bash
 
 apt-get update
@@ -79,168 +82,169 @@ su ubuntu -c 'cd /home/ubuntu && nohup ./launch-mcs.sh >> gdxsv.log &'
 }
 
 function forResponse(vm) {
-  const v = {};
-  try {
-    v.name = vm.name;
-    v.zone = vm.metadata.zone;
-    v.created = vm.metadata.creationTimestamp;
-    v.status = vm.metadata.status;
-    v.tags = vm.metadata.tags.items;
-    v.nat_ip = vm.metadata.networkInterfaces[0].accessConfigs[0].natIP;
-  } catch (e) {
-    console.log(e);
-  }
-  return v;
+    const v = {};
+    try {
+        v.name = vm.name;
+        v.zone = vm.metadata.zone;
+        v.created = vm.metadata.creationTimestamp;
+        v.status = vm.metadata.status;
+        v.tags = vm.metadata.tags.items;
+        v.nat_ip = vm.metadata.networkInterfaces[0].accessConfigs[0].natIP;
+    } catch (e) {
+        console.log(e);
+    }
+    return v;
 }
 
-exports.cloudFunctionEntryPoint = async (req, res) => {
-  // require GOOGLE_APPLICATION_CREDENTIALS environment variable
-  const Compute = require('@google-cloud/compute');
-  const url = require('url');
+async function getList(req, res) {
+    const compute = new Compute();
 
-  console.log(req.url);
-
-  const query = url.parse(req.url, true).query
-  const compute = new Compute();
-
-  if (req.method != "GET") {
-    res.status(400).send('bad request');
-    return;
-  }
-
-  if (req.url.startsWith("/list")) {
     const [vms] = await compute.getVMs({
-      autoPaginate: false,
-      maxResults: 100,
-      filter: "name eq gdxsv-mcs.*",
+        autoPaginate: false,
+        maxResults: 100,
+        filter: "name eq gdxsv-mcs.*",
     });
 
     const vmlist = [];
     for (let i = 0; i < vms.length; i++) {
-      vmlist.push(forResponse(vms[i]));
+        vmlist.push(forResponse(vms[i]));
     }
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(vmlist, null, "  "));
     return;
-  }
+}
 
-  if (req.url.startsWith("/regions")) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(gcpRegions, null, "  "));
-    return;
-  }
+async function getDeleteAll(req, res) {
+    const compute = new Compute();
 
-  if (req.url.startsWith("/deleteall")) {
-    // That's for debugging
     const [vms] = await compute.getVMs({
-      autoPaginate: false,
-      maxResults: 100,
-      filter: "name eq gdxsv-mcs.*",
+        autoPaginate: false,
+        maxResults: 100,
+        filter: "name eq gdxsv-mcs.*",
     });
 
     const vmlist = [];
     const deletes = [];
-    for (let i = 0; i < vms.length; i++) {
-      const [operation] = await vms[i].delete()
-      deletes.push(operation.promise);
-      vmlist.push(forResponse(vms[i]));
+    for (let vm of vms) {
+        const [operation] = await vm.delete()
+        deletes.push(operation.promise);
+        vmlist.push(forResponse(vm));
     }
     await Promise.all(deletes);
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(vmlist, null, "  "));
-    return;
-  }
+}
 
-  if (req.url.startsWith("/alloc")) {
+
+async function getAlloc(req, res) {
+    const compute = new Compute();
+    const query = url.parse(req.url, true).query
+
     const region = query["region"];
     const version = query["version"] ? query["version"] : "latest";
     const regionInfo = gcpRegions[region];
     const vmName = "gdxsv-mcs-" + region + "-" + version.replace(/\./g, "-")
 
     if (!regionInfo) {
-      res.status(400).send('invalid region');
-      return;
+        res.status(400).send('invalid region');
+        return;
     }
 
     let [vms] = await compute.getVMs({
-      autoPaginate: false,
-      maxResults: 100,
-      filter: "name eq " + vmName,
+        autoPaginate: false,
+        maxResults: 100,
+        filter: "name eq " + vmName,
     })
 
     console.log("" + vms.length + "vms found.");
 
     let vm = vms.find(vm => vm.metadata.status == "RUNNING");
     if (vm) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(forResponse(vm), null, "  "));
-      return;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(forResponse(vm), null, "  "));
+        return;
     }
 
     console.log("running vm not found");
 
     for (let vm of vms.filter(vm => vm.metadata.status == "TERMINATED")) {
-      try {
-        console.log("starting vm...", vm);
-        let [operation] = await vm.setMetadata({
-          'startup-script': getStartupScript(version),
-        });
-        await operation.promise();
-        [operation] = await vm.start();
-        await operation.promise();
-        console.log("start vm done");
-        [vm.metadata] = await vm.waitFor("RUNNING", { timeout: 30 });
-        console.log("wait done");
-      } catch (e) {
-        console.log(e);
-        continue;
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(forResponse(vm), null, "  "));
-      return;
+        try {
+            console.log("starting vm...", vm);
+            let [operation] = await vm.setMetadata({
+                'startup-script': getStartupScript(version),
+            });
+            await operation.promise();
+            [operation] = await vm.start();
+            await operation.promise();
+            console.log("start vm done");
+            [vm.metadata] = await vm.waitFor("RUNNING", {timeout: 30});
+            console.log("wait done");
+        } catch (e) {
+            console.log(e);
+            continue;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(forResponse(vm), null, "  "));
+        return;
     }
 
     console.log("no available vm found in", region);
 
     for (let z of regionInfo.zones) {
-      const zoneName = region + "-" + z;
-      try {
-        console.log("trying to create new vm in", zoneName);
-        const zone = compute.zone(zoneName);
-        const [vm, operation] = await zone.createVM(vmName, {
-          os: "ubuntu",
-          http: true,
-          tags: ["gdxsv-mcs"],
-          machineType: "g1-small",
-          scheduling: { preemptible: true },
-          metadata: {
-            items: [
-              { key: "startup-script", value: getStartupScript(version) },
-            ],
-          },
-        });
+        const zoneName = region + "-" + z;
+        try {
+            console.log("trying to create new vm in", zoneName);
+            const zone = compute.zone(zoneName);
+            const [vm, operation] = await zone.createVM(vmName, {
+                os: "ubuntu",
+                http: true,
+                tags: ["gdxsv-mcs"],
+                machineType: "g1-small",
+                scheduling: {preemptible: true},
+                metadata: {
+                    items: [
+                        {key: "startup-script", value: getStartupScript(version)},
+                    ],
+                },
+            });
 
-        await operation.promise();
-        console.log("vm created");
-        const [metadata] = await vm.waitFor("RUNNING", { timeout: 30 });
-        vm.metadata = metadata
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(forResponse(vm), null, "  "));
-        console.log("wait done");
-        return;
-      } catch (e) {
-        console.log(e);
-        continue;
-      }
+            await operation.promise();
+            console.log("vm created");
+            const [metadata] = await vm.waitFor("RUNNING", {timeout: 30});
+            vm.metadata = metadata
+            console.log("new vm is running", vm);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(forResponse(vm), null, "  "));
+            return;
+        } catch (e) {
+            console.log(e);
+            continue;
+        }
     }
 
     console.log('failed to allocate vm');
     res.status(503).send('failed to allocate vm');
     return;
-  }
+}
 
-  res.status(400).send('bad request');
-  return;
+
+exports.cloudFunctionEntryPoint = async (req, res) => {
+    if (req.method != "GET") {
+        res.status(400).send('bad request');
+        return;
+    }
+
+    if (req.url.startsWith("/list")) {
+        return await getList(req, res);
+    }
+    if (req.url.startsWith("/deleteall")) {
+        return await getDeleteAll(req, res);
+    }
+    if (req.url.startsWith("/alloc")) {
+        return await getAlloc(req, res);
+    }
+
+    res.status(400).send('bad request');
 }
