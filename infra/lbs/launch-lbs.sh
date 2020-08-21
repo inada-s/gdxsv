be5143b576a895cb4a7c3197a7a12c125e7b1673
Shingo INADA <mecha.is06@gmail.com>
2020-08-21 20:47:03 +0900

    add service files

--- infra/lbs/launch-lbs.sh
+++ infra/lbs/launch-lbs.sh
@@ -2,27 +2,28 @@
 
 set -eux
 
-cd $(dirname $0)
+cd $(dirname "$0")
 
-# Download latest version from github release
 readonly LATEST_TAG=$(curl -sL https://api.github.com/repos/inada-s/gdxsv/releases/latest | jq -r '.tag_name')
 readonly DOWNLOAD_URL=$(curl -sL https://api.github.com/repos/inada-s/gdxsv/releases/latest | jq -r '.assets[].browser_download_url')
 
 if [[ ! -d $LATEST_TAG/bin ]]; then
   echo "Downloading latest version..."
-  mkdir -p $LATEST_TAG
-  pushd $LATEST_TAG
-    wget $DOWNLOAD_URL
+  mkdir -p "$LATEST_TAG"
+  pushd "$LATEST_TAG"
+    wget "$DOWNLOAD_URL"
     tar xzvf bin.tgz && rm bin.tgz
   popd
 fi
 
-PUBLIC_IP=$(curl -s https://ipinfo.io/ip)
-export GDXSV_LOBBY_PUBLIC_ADDR="${PUBLIC_IP}:9876"
+GDXSV_BIN=$LATEST_TAG/bin/gdxsv
+
+export GDXSV_LOBBY_PUBLIC_ADDR="153.121.44.150:9876"
 export GDXSV_LOBBY_ADDR=":9876"
-export GDXSV_BATTLE_PUBLIC_ADDR="${PUBLIC_IP}:9877"
+export GDXSV_BATTLE_PUBLIC_ADDR="153.121.44.150:9877"
 export GDXSV_BATTLE_ADDR=":9877"
 export GDXSV_DB_NAME="gdxsv.db"
+export GDXSV_MCSFUNC_KEY="/etc/google/auth/application_default_credentials.json"
+export GDXSV_MCSFUNC_URL="https://asia-northeast1-gdxsv-274515.cloudfunctions.net/mcsfunc"
 
-# TODO: remove -v=3
-exec $LATEST_TAG/bin/gdxsv lbs -v=3
+exec "$GDXSV_BIN" -prodlog lbs >> /var/log/gdxsv-lbs.log 2>&1
