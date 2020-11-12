dbb258d643e9b1b16db2ea97327908b97e261f5c
Shingo INADA <mecha.is06@gmail.com>
2020-11-12 15:23:50 +0900

    Updates

---
+++ precache-manifest.e0ad16f15ff1ba72a658fb5dd3a2fdd8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2996bc77dd8c7c67fc010d603382d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f229b866ec62e3e7b68",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c2b947e7f7d66202b5de",
+    "url": "/static/css/main.b5d4cbc5.chunk.css"
+  },
+  {
+    "revision": "3f229b866ec62e3e7b68",
+    "url": "/static/js/2.ef406cf5.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.ef406cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b947e7f7d66202b5de",
+    "url": "/static/js/main.5d44eb97.chunk.js"
+  },
+  {
+    "revision": "471319ced4ce265e61e8",
+    "url": "/static/js/runtime-main.b54dcca1.js"
+  },
+  {
+    "revision": "7fcf2fb844f47071335078cf67c2afc1",
+    "url": "/static/media/flycast.7fcf2fb8.png"
+  },
+  {
+    "revision": "fff3aa6702c3ac85d38b0575f3b523bc",
+    "url": "/static/media/game.fff3aa67.jpg"
+  },
+  {
+    "revision": "d5ffaa2056e5287b2f9475ca185a0d28",
+    "url": "/static/media/headset.d5ffaa20.jpg"
+  },
+  {
+    "revision": "f7923533a80fa49ccd80f09e111fedd7",
+    "url": "/static/media/stick.f7923533.jpg"
+  },
+  {
+    "revision": "aa1bc33f9daedf39b083ecdc234f3379",
+    "url": "/static/media/title.aa1bc33f.png"
+  },
+  {
+    "revision": "06a461465e286a2d82b3ec0051ac45dd",
+    "url": "/static/media/world.06a46146.jpg"
+  }
+]);
