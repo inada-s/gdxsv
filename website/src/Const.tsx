50ad3fa91acb9831a7f83b714e513fd621457462
Shingo INADA <mecha.is06@gmail.com>
2020-11-12 22:49:46 +0900

    fix website url

--- website/src/Const.tsx
+++ website/src/Const.tsx
@@ -1,4 +1,4 @@
 // @ts-ignore
-export const WebsiteUrl = "https://inada-s.github.io/gdxsv"
-export const DiscordUrl = "https://discord.gg/ktRw4JX"
+export const WebsiteUrl = "https://gdxsv.net";
+export const DiscordUrl = "https://discord.gg/ktRw4JX";
 export const GuideJaUrl = "https://gdoc.pub/doc/1o0ab6vRduUVefKC4GE7r2zhNfNomVKL8DdDQtx_ZBbA#?usp=sharing";
