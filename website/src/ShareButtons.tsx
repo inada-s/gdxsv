5a67d942d4dfc5318ca6740f861468b593dedd69
Ming Chan <stupidming@gmail.com>
2021-01-18 19:37:02 +0000

    Update Translation for share button

--- website/src/ShareButtons.tsx
+++ website/src/ShareButtons.tsx
@@ -22,7 +22,7 @@ const Link = ({ href, ariaLabel, children }: ILink) => (
 
 const TwitterButton = () => {
     const intl = useIntl();
-    const text = intl.formatMessage({ id: "footer.tweet.data-text" });
+    const text = intl.formatMessage({ id: "share.data-text" });
     const url = escape(WebsiteUrl);
     const hashTags = ([
         'gdxsv',
@@ -33,7 +33,7 @@ const TwitterButton = () => {
     const href = `https://twitter.com/share?text=${text}&url=${url}&hashtags=${hashTags}`;
 
     return (
-        <Link href={href} ariaLabel="Share on Twitter">
+        <Link href={href} ariaLabel={intl.formatMessage({ id: "share.twitter.label" })}>
             <svg style={{ width: '2rem' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z" /></svg>
         </Link>
     );
@@ -43,7 +43,7 @@ const FacebookButton = () => {
     const intl = useIntl();
     const href = `https://facebook.com/sharer/sharer.php?u=${escape(WebsiteUrl)}`;
     return (
-        <Link href={href} ariaLabel={intl.formatMessage({ id: "footer.facebook.title" })}>
+        <Link href={href} ariaLabel={intl.formatMessage({ id: "share.facebook.label" })}>
             <svg style={{ width: '2rem' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z" /></svg>
         </Link>
     );
@@ -51,11 +51,11 @@ const FacebookButton = () => {
 
 const MeweButton = () => {
     const intl = useIntl();
-    const text = encodeURI(intl.formatMessage({ id: "footer.tweet.data-text" }));
+    const text = encodeURI(intl.formatMessage({ id: "share.data-text" }));
     const url = escape(WebsiteUrl);
     const href = `https://mewe.com/share?link=${url}%20${text}`;
     return (
-        <Link href={href} ariaLabel="Share on Twitter">
+        <Link href={href} ariaLabel={intl.formatMessage({ id: "share.mewe.label" })}>
             <svg style={{ width: '2rem' }} xmlns="http://www.w3.org/2000/svg" viewBox="-3 -3 30 30">
                 <clipPath id="circle">
                     <circle cx="12" cy="12" r="15" />
@@ -69,10 +69,11 @@ const MeweButton = () => {
 };
 
 const LineButton = () => {
+    const intl = useIntl();
     const url = escape(WebsiteUrl);
     const href = `line://msg/text/${url}`;
     return (
-        <Link href={href} ariaLabel="Share on LINE">
+        <Link href={href} ariaLabel={intl.formatMessage({ id: "share.line.label" })}>
             <svg style={{ width: '2rem' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 -6 128 128">
                 <clipPath id="circle">
                     <circle cx="64" cy="58" r="64" />
