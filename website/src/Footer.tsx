eb5c85f57043c43da9aeededb842cc04158ccfb8
Ming Chan <stupidming@gmail.com>
2021-01-18 19:13:25 +0000

    Add facebook Button

--- website/src/Footer.tsx
+++ website/src/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
 import { WebsiteUrl } from './Const';
-import { TwitterButton } from "./ShareButtons";
+import { TwitterButton, FacebookButton } from "./ShareButtons";
 
 export default function Footer() {
     const intl = useIntl();
@@ -9,6 +9,7 @@ export default function Footer() {
         <footer>
             <div className={"d-flex justify-content-center my-5"}>
                 <TwitterButton />
+                <FacebookButton />
             </div>
             <p>{intl.formatMessage({ id: "footer.copyright" })}</p>
         </footer>
