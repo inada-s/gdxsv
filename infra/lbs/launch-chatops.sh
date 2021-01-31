51546a9a5febe1cdc0796c86826286fb71125f83
Shingo INADA <mecha.is06@gmail.com>
2021-01-31 19:42:49 +0900

    fix script

--- infra/lbs/launch-chatops.sh
+++ infra/lbs/launch-chatops.sh
@@ -4,11 +4,10 @@ set -eux
 
 cd $(dirname "$0")
 
-if [[ ! -d $LATEST_TAG/bin ]]; then
+if [[ ! -d venv ]]; then
   python3 -m venv venv
 fi
 
 source venv/bin/activate
 python3 -m pip install -r chatops/requirements.txt
 exec python3 chatops/bot.py
-
