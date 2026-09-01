6af41d9ac54f9cbd96013dec06f6e9cdc0719510
Edward Li <vk.edward.li@gmail.com>
2026-09-01 19:11:06 +0800

    mcs: fix lint

--- gdxsv/mcs.go
+++ gdxsv/mcs.go
@@ -114,17 +114,14 @@ func (mcs *Mcs) ListenAndServe(addr string) {
 	udpSv := NewUDPServer(mcs)
 
 	go func(addr string) {
+		// Only returns on failure.
 		err := tcpSv.ListenAndServe(addr)
-		if err != nil {
-			logger.Fatal("tcpSv.ListenAndServe", zap.Error(err))
-		}
+		logger.Fatal("tcpSv.ListenAndServe", zap.Error(err))
 	}(addr)
 
 	go func(addr string) {
 		err := udpSv.ListenAndServe(addr)
-		if err != nil {
-			logger.Fatal("udpSv.ListenAndServe", zap.Error(err))
-		}
+		logger.Fatal("udpSv.ListenAndServe", zap.Error(err))
 	}(addr)
 }
 
